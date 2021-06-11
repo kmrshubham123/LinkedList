a9dd7291735ca2ed7a3fec1197cf72cbb2d970ba
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-11 22:35:17 +0530

    [Kumar Shubham] Added: Created simple linked list

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+++++Welcome to the Linked List+++++");
+            SingleLinkedList list = new SingleLinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+           
+            
+           
+        }
+    }
+}
---
+++ LinkedList/LinkedList/SingleLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class SingleLinkedList
+    {
+        Node head; //starting of linked list defining position head
+        public void Add(int data)// proprty of adding (add the element of linked list)
+        {
+            Node node = new Node(data);//  creating the object of the node class
+                                       //  and pushing data into it
+            if (head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp= temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Inserted into Linked List", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                
+                temp = temp.next;
+            }
+        }
+
+
+    }
+   
+}
